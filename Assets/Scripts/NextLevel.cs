957198096a7145aa092636759f171c1c084046c9
Yago Carro <ycarro@student.42madrid.com>
2021-10-17 17:03:53 +0200

    with_menu

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -2,33 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-     
-    /*public class Example
-    {
-        public void LoadLevel()
-    {
-            // load the nextlevel
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-     
-        }
-    }*/
 
 
 public class NextLevel : MonoBehaviour
 {
-    int level;
+   int level;
+
     // Start is called before the first frame update
     void Start()
     {
-        level = PlayerPrefs.GetInt("currentLevel", 0);
-        //Debug.Log(PlayerPrefs.GetInt("currentLevel"));
-        //SceneManager.LoadScene(level);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    
+        level = PlayerPrefs.GetInt("currentLevel");
     }
 
     void OnTriggerEnter(Collider collider)

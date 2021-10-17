913c2128212276683caade57db0273664e55155e
Yago Carro <ycarro@student.42madrid.com>
2021-10-17 18:47:42 +0200

    added_pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool IsPaused = false;
+	public GameObject PauseMenuUI;
+    public Button bResume;
+    public Button bRestart;
+    public Button bExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bResume.GetComponent<Button>().onClick.AddListener(Resume);
+        bRestart.GetComponent<Button>().onClick.AddListener(RestartLevel);
+        bExit.GetComponent<Button>().onClick.AddListener(ExitToMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESX");
+        	if (IsPaused)
+        	{
+        		Resume();
+        	}
+        	else
+        	{
+        		Pause();
+        	}
+        }
+    }
+
+    void Resume()
+    {
+    	PauseMenuUI.SetActive(false);
+    	//Time.timeScale = 1f;
+    	IsPaused = false;
+    }
+
+    void Pause()
+    {
+    	PauseMenuUI.SetActive(true);
+    	//Time.timeScale = 0f;
+    	IsPaused = true;
+    }
+
+    void RestartLevel()
+    {
+        IsPaused = false;
+        SceneManager.LoadScene(PlayerPrefs.GetInt("currentLevel"));
+    }
+
+    void ExitToMenu()
+    {
+        IsPaused = false;
+        SceneManager.LoadScene(0);
+    }
+
+}
